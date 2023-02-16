68dac0521db34a3bf1b8f80593c022240614289b
FelixHoff1988 <*YKfM29QxNZS>
2023-02-16 11:09:27 +0100

    section 5 done

---
+++ Variablen/Kommentare/main.js
@@ -0,0 +1,8 @@
+"use strict";
+
+// gibt "Hallo" in die Konsole aus - als Kommentar zu unserem Code
+// console.log("Hallo"); - zum auskommentieren von Code
+
+/*
+Das ist ein mehrzeiliger Kommentar.
+*/
