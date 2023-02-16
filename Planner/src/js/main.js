a155f4389ee64325574c06889ed01ee23508b3eb
FelixHoff1988 <*YKfM29QxNZS>
2023-02-16 22:23:15 +0100

    section 10 done

--- Planner/src/js/main.js
+++ Planner/src/js/main.js
@@ -1,5 +1,9 @@
 "use strict";
 
+let einnahmen = 0;
+let ausgaben = 0;
+let bilanz = 0;
+
 // Eingabedaten holen
 let titel_1 = prompt("Titel:");
 let typ_1 = prompt("Typ (Einnahme oder Ausgabe):");
@@ -12,6 +16,15 @@ Betrag: ${betrag_1} ct
 Datum: ${datum_1}`
 );
 
+if (typ_1 == "Einnahme"){
+    einnahmen += betrag_1;
+    bilanz += betrag_2;
+} else if (typ_1 == "Ausgabe") {
+    ausgaben += betrag_1;
+    bilanz -= betrag_1;
+} else {
+    console.log(`Der Typ von "${typ_1}" ist nicht bekannt.`);
+}
 
 let titel_2 = prompt("Titel:");
 let typ_2 = prompt("Typ (Einnahme oder Ausgabe):");
@@ -23,11 +36,20 @@ Betrag: ${betrag_2} ct
 Datum: ${datum_2}`
 );
 
-// Gesamtbilanz
-let einnahmen;
-let ausgaben;
-let bilanz = betrag_1 + betrag_2;
-console.log(`Bilanz: ${bilanz} ct`);
+if (typ_2 == "Einnahme"){
+    einnahmen += betrag_2;
+    bilanz += betrag_2;
+} else if (typ_2 == "Ausgabe") {
+    ausgaben += betrag_2;
+    bilanz -= betrag_2;
+} else {
+    console.log(`Der Typ von "${typ_2}" ist nicht bekannt.`);
+}
 
 let positiv = bilanz >= 0;
-console.log(`Bilanz ist positiv: ${positiv}`);
+
+console.log(`Einnahmen: ${einnahmen} ct
+Ausgaben: ${ausgaben} ct
+Bilanz: ${bilanz} ct
+Bilanz ist positiv: ${positiv}`
+);
