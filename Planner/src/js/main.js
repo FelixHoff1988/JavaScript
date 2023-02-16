c453f07e8fa83bcd3342d6ad4f4bb8efebde3fda
FelixHoff1988 <*YKfM29QxNZS>
2023-02-16 22:05:38 +0100

    updated console logs

--- Planner/src/js/main.js
+++ Planner/src/js/main.js
@@ -2,22 +2,26 @@
 
 // Eingabedaten holen
 let titel_1 = prompt("Titel:");
-console.log(`Titel: ${titel_1}`);
 let typ_1 = prompt("Typ (Einnahme oder Ausgabe):");
-console.log(`Typ: ${typ_1}`);
 let betrag_1 = parseInt(prompt("Betrag (in Cent):"));
-console.log(`Betrag: ${betrag_1} ct`);
 let datum_1 = prompt("Datum (jjjj-mm-tt):");
-console.log(`Datum: ${datum_1}`);
+
+console.log(`Titel: ${titel_1}
+Typ: ${typ_1}
+Betrag: ${betrag_1} ct
+Datum: ${datum_1}`
+);
+
 
 let titel_2 = prompt("Titel:");
-console.log(`Titel: ${titel_2}`);
 let typ_2 = prompt("Typ (Einnahme oder Ausgabe):");
-console.log(`Typ: ${typ_2}`);
 let betrag_2 = parseInt(prompt("Betrag (in Cent):"));
-console.log(`Betrag: ${betrag_2} ct`);
 let datum_2 = prompt("Datum (jjjj-mm-tt):");
-console.log(`Datum: ${datum_2}`);
+console.log(`Titel: ${titel_2}
+Typ: ${typ_2}
+Betrag: ${betrag_2} ct
+Datum: ${datum_2}`
+);
 
 // Gesamtbilanz
 let einnahmen;
