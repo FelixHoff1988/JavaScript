6ddb6049afdd0dd093d406f28dc8eaa50a706d36
FelixHoff1988 <felixjacobhoff@gmail.com>
2023-02-18 09:03:08 +0100

    section 16 done

--- Planner/src/js/main.js
+++ Planner/src/js/main.js
@@ -29,14 +29,18 @@ Datum: ${this.neuer_eintrag.datum}`);
     },
 
     eintrag_mit_gesamtbilanz_verrechnene() {
-        if (this.neuer_eintrag.typ == "Einnahme") {
-            this.gesamtbilanz.einnahmen += this.neuer_eintrag.betrag;
-            this.gesamtbilanz.bilanz += this.neuer_eintrag.betrag;
-        } else if (this.neuer_eintrag.typ == "Ausgabe") {
-            this.gesamtbilanz.ausgaben += this.neuer_eintrag.betrag;
-            this.gesamtbilanz.bilanz -= this.neuer_eintrag.betrag;
-        } else {
-            console.log(`Der Typ von "${this.neuer_eintrag.typ}" ist nicht bekannt.`);
+        switch (this.neuer_eintrag.typ) {
+            case "Einnahme":
+                this.gesamtbilanz.einnahmen += this.neuer_eintrag.betrag;
+                this.gesamtbilanz.bilanz += this.neuer_eintrag.betrag;
+                break;
+            case "Ausgabe":
+                this.gesamtbilanz.ausgaben += this.neuer_eintrag.betrag;
+                this.gesamtbilanz.bilanz -= this.neuer_eintrag.betrag;
+                break;
+            default:
+                console.log(`Der Typ von "${this.neuer_eintrag.typ}" ist nicht bekannt.`);
+                break;
         }
     },
     gesamtbilanz_ausgeben() {
@@ -59,4 +63,3 @@ Bilanz ist positiv: ${this.gesamtbilanz.bilanz >= 0}`
 haushaltsbuch.eintrag_hinzufügen();
 haushaltsbuch.eintrag_hinzufügen();
 haushaltsbuch.eintrag_hinzufügen();
-
