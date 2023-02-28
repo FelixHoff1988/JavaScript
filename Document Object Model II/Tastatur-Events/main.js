00abbda5e33817f37f6d07d4b4b6ddb37964da12
FelixHoff1988 <felixjacobhoff@gmail.com>
2023-02-28 17:34:15 +0100

    formular-events

---
+++ Document Object Model II/Tastatur-Events/main.js
@@ -0,0 +1,21 @@
+"use strict";
+
+document.addEventListener("keydown", e => {
+    //console.log(e);
+    if (e.key === "@") {
+        console.log("Das @-Zeichen wurde gedrückt!");
+    }
+});
+
+document.addEventListener("keyup", e => {
+    if (e.key === "p") {
+        console.log("Das p-Zeichen wurde gedrückt!");
+    }
+});
+
+// wichtig: keypress-Event hört nicht auf Shift, Fn und Capslock
+document.addEventListener("keypress", e => {
+    if (e.key === ":") {
+        console.log("Das :-Zeichen wurde gedrückt!");
+    }
+});
